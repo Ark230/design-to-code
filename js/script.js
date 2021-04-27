c0158959c32bd8307194fb6e8f676eb209f90d53
Ignacio Pérez Cumillaf <iperezc99@gmail.com>
2021-04-27 18:56:43 -0400

    notifications were added for both mobile and desktop; form behavior with js added too

--- js/script.js
+++ js/script.js
@@ -14,6 +14,50 @@ document.addEventListener("DOMContentLoaded", function() {
     const registerLink = document.getElementById("register-link");
     const registerButton = document.getElementById("register-button");
 
+    const signInLinkMobile = document.getElementById("sign-in-link-mobile");
+    const signInLinkMobile2 = document.getElementById("sign-in-link-mobile-2");
+
+    const registerLinkMobile = document.getElementById("register-link-mobile");
+    const registerLinkMobile2 = document.getElementById("register-link-mobile-2");
+
+    const desktopRegisterForm = document.getElementById("register-form-desktop");
+    const desktopRegisterMobile = document.getElementById("register-form-mobile");
+
+    function preventSubmitAndDeployNotification(event, device){
+        event.preventDefault();
+        
+        switch (device) {
+            case "desktop":
+                desktopRegisterForm.reset();
+                toggle(document.getElementById("user-credentials-register"));
+                toggle(document.getElementById("user-credentials-sign-in"));
+                toggle(document.getElementById("notification"));
+                setTimeout(() => {
+                    //aparece la notificacion, se oculta el registro y se muestra el login
+                    toggle(document.getElementById("notification"));
+                    
+                }, 5000)
+                break;
+            case "phone":
+                desktopRegisterMobile.reset();
+                toggle(document.getElementById("user-credentials-mobile-register"));
+                toggle(document.getElementById("user-credentials-mobile-sign-in"));
+                toggle(document.getElementById("notification-mobile"));
+                setTimeout(() => {
+                    //aparece la notificacion, se oculta el registro y se abre el login
+                    toggle(document.getElementById("notification-mobile"));
+                    
+                }, 5000)
+                break;
+        }
+    
+    }
+
+    desktopRegisterForm.onsubmit = (event) => {preventSubmitAndDeployNotification(event, "desktop")}
+    desktopRegisterMobile.onsubmit = (event) => {preventSubmitAndDeployNotification(event, "phone")}
+
+
+    //DESKTOP
     signInLink.addEventListener("click", () => {
         toggle(document.getElementById("user-credentials-sign-in"));
         toggle(document.getElementById("user-credentials-register"));
@@ -26,18 +70,42 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 
-    registerButton.addEventListener("click", () => {
-        toggle(document.getElementById("user-credentials-register"));
-        toggle(document.getElementById("user-credentials-sign-in"));
-
-        // toggle(document.getElementById("notification"));
-
+    // registerButton.addEventListener("click", () => {
+    //     toggle(document.getElementById("user-credentials-register"));
+    //     toggle(document.getElementById("user-credentials-sign-in"));
+    // });
 
 
+
+    //MOBIlE
+    registerLinkMobile.addEventListener("click", () => {
+        toggle(document.getElementById("login-mobile"));
+        toggle(document.getElementById("user-credentials-mobile-register"));
     });
 
+    registerLinkMobile2.addEventListener("click", () => {
+        toggle(document.getElementById("user-credentials-mobile-sign-in"));
+        toggle(document.getElementById("user-credentials-mobile-register"));
+        
+    });
 
+    signInLinkMobile.addEventListener("click", () => {
+        toggle(document.getElementById("user-credentials-mobile-sign-in"));
+        toggle(document.getElementById("user-credentials-mobile-register"));
+    });
 
+    // signInLinkMobile2.addEventListener("click", () => {
+    //     toggle(document.getElementById("user-credentials-mobile-register"));
+    //     toggle(document.getElementById("user-credentials-mobile-sign-in"));
+    // });
+    
 
+    
 
   });
+
+
+
+  
+
+  
